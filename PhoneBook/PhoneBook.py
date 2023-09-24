3f04fe4c82fc7b8427b0e6314e5359cdddaf3706
Alex <sp.savinov@gmail.com>
2023-09-24 10:11:53 +0300

    restyle

--- PhoneBook/PhoneBook.py
+++ PhoneBook/PhoneBook.py
@@ -1,15 +1,13 @@
 # -*- coding: cp1251 -*-
 
-#  Создаем файл 
+
 from pickle import TRUE
-
-
 def create_file():
     with open('file.txt','w') as data:
         data.write('Имя, Фамилия, 892020 \n')
         data.write('Имя, Фамилия, 892020 \n')
 
-#  Читаем файл 
+ 
 def read_file():
       path = 'file.txt'
       data = open('file.txt', 'r')
@@ -17,19 +15,17 @@ def read_file():
             print(line, end ='')
       data.close
 
-#  Ввод данных 
+
 def write_data():
       second_name  = input("Введите фамилию: ") 
       first_name = input("Введите имя: ")    
       phone = input("Введите номер телефона: ")   
       write_person(second_name, first_name, phone)
 
-#  Запись данных 
 def write_person(second_name, first_name, phone):
       with open('file.txt','a') as f:
             f.write(f'\n {second_name}, {first_name}, {phone}')
 
-#  Поиск человека
 def find_person():
       second_name  = input("Введите фамилию: ") 
       first_name = input("Введите имя: ")    
